dbea14c5506d7138303e558810308ed57f203727
phillipdunn <phillipdunn@hotmail.com>
2024-07-28 19:51:39 +0100

    feat: add server logic

--- server/server.py
+++ server/server.py
@@ -1,13 +1,28 @@
 import requests
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def api():
-  return 'API is working'
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file:
+        upload_dir = 'uploads'
+        os.makedirs(upload_dir, exist_ok=True)
+        
+        file.save(os.path.join(upload_dir, file.filename))
+        return jsonify({'message': 'File uploaded successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
